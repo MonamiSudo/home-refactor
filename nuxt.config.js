ae971d6f8943c7b9ddc254f14564ce242dec1dc6
須藤百波 <sudomonami@gmail.com>
2022-04-14 12:55:43 +0900

    共通のscssファイル作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,7 +67,8 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/style.css', // tailwindで新しくユーティリティを追加したCSS
-    'swiper/css/swiper.css' 
+    'swiper/css/swiper.css' ,
+    '@/assets/scss/style.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
