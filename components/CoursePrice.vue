3452e6072f57b1a76f471421312366ead9f4a4d9
須藤百波 <sudomonami@gmail.com>
2023-01-25 16:24:45 +0900

    産後lpの価格表が初回が目立つように修正。

--- components/CoursePrice.vue
+++ components/CoursePrice.vue
@@ -12,24 +12,27 @@
           <ul class="flex overflow-x-auto gap-x-8 sp:gap-x-6 mt-6 justify-center sp:justify-start">
             <li class="border-2 border-gray-300 text-center rounded-lg pb-7 mt-8 min-w-max">
               <p class="px-10 sp:px-8 bg-gray-300 py-4 text-3xl text-white">60<span class="text-base">分</span></p>
-              <p class="px-10 sp:px-8 mt-7 text-2xl">{{ course.price.time60.toLocaleString() }}<span class="text-base">円（税込）</span></p>
-              <p class="px-10 sp:px-8 mt-10 md:mt-12 text-sm">初回ご利用のお客様</p>
-              <p class="px-10 sp:px-8 mt-1 mb-10 md:mb-12 text-xl">{{ halfPrice(course.price.time60) }}<span class="text-base">円（税込）</span></p>
-              <a class="py-3 mx-auto w-4/5 block bg-gray-300 border-2 border-gray-300 rounded-lg text-white hover:text-gray-300 hover:bg-white" href="https://lin.ee/zTsXoxq" @click="homeAddLine()" target="_blank">コースを予約する</a>
+              <p class="px-10 sp:px-8 mt-7 md:mt-12 text-sm">初回コース料金半額</p>
+              <p class="px-10 sp:px-8 text-3xl">{{ halfPrice(course.price.time60) }}<span class="text-base">円（税込）</span></p>
+              <p class="px-10 sp:px-8 mt-7 md:mt-12 text-sm">通常価格</p>
+              <p class="px-10 sp:px-8 mb-10 md:mb-12 text-base">{{ course.price.time60.toLocaleString() }}<span class="text-sm">円（税込）</span></p>
+              <a class="py-3 mx-auto w-4/5 block bg-gray-300 border-2 border-gray-300 rounded-lg text-white hover:text-gray-300 hover:bg-white" href="https://lin.ee/zTsXoxq" @click="homeAddLine()" target="_blank">LINEで予約する</a>
             </li>
             <li class="border-4 border-red-400 text-center rounded-lg pb-7 min-w-max">
-              <p class="px-10 sp:px-8 bg-red-400 py-8 text-3xl text-white">90<span class="text-base">分</span></p>
-              <p class="px-10 sp:px-8 mt-7 text-2xl">{{ course.price.time90.toLocaleString() }}<span class="text-base">円（税込）</span></p>
-              <p class="px-10 sp:px-8 mt-10 md:mt-12 text-sm">初回ご利用のお客様</p>
-              <p class="px-10 sp:px-8 mt-1 mb-10 md:mb-12 text-xl">{{ halfPrice(course.price.time90) }}<span class="text-base">円（税込）</span></p>
-              <a class="py-3 mx-auto w-4/5 block bg-red-400 border-2 border-red-400 rounded-lg text-white hover:text-red-400 hover:bg-white" href="https://lin.ee/zTsXoxq" @click="homeAddLine()" target="_blank">コースを予約する</a>
+              <p class="px-10 sp:px-8 bg-red-400 py-8 text-3xl text-white font-bold tracking-wider">90<span class="text-base">分</span></p>
+              <p class="px-10 sp:px-8 mt-7 md:mt-12 text-sm">初回コース料金半額</p>
+              <p class="px-10 sp:px-8 font-bold text-3xl tracking-wide">{{ halfPrice(course.price.time90) }}<span class="text-base">円（税込）</span></p>
+              <p class="px-10 sp:px-8 mt-7 md:mt-12 text-sm">通常価格</p>
+              <p class="px-10 sp:px-8 mb-10 md:mb-12 text-base">{{ course.price.time90.toLocaleString() }}<span class="text-sm">円（税込）</span></p>
+              <a class="btn-90 relative py-3 mx-auto w-4/5 block bg-red-400 border-2 border-red-400 rounded-lg text-white hover:text-red-400 hover:bg-white" href="https://lin.ee/zTsXoxq" @click="homeAddLine()" target="_blank">LINEで予約する</a>
             </li>
             <li class="border-2 border-gray-300 text-center rounded-lg pb-7 mt-8 min-w-max">
               <p class="px-10 sp:px-8 bg-gray-300 py-4 text-3xl text-white">120<span class="text-base">分</span></p>
-              <p class="px-10 sp:px-8 mt-7 text-2xl">{{ course.price.time120.toLocaleString() }}<span class="text-base">円（税込）</span></p>
-              <p class="px-10 sp:px-8 mt-10 md:mt-12 text-sm">初回ご利用のお客様</p>
-              <p class="px-10 sp:px-8 mt-1 mb-10 md:mb-12 text-xl">{{ halfPrice(course.price.time120) }}<span class="text-base">円（税込）</span></p>
-              <a class="py-3 mx-auto w-4/5 block bg-gray-300 border-2 border-gray-300 rounded-lg text-white hover:text-gray-300 hover:bg-white" href="https://lin.ee/zTsXoxq" @click="homeAddLine()" target="_blank">コースを予約する</a>
+              <p class="px-10 sp:px-8 mt-7 md:mt-12 text-sm">初回コース料金半額</p>
+              <p class="px-10 sp:px-8 text-3xl">{{ halfPrice(course.price.time120) }}<span class="text-base">円（税込）</span></p>
+              <p class="px-10 sp:px-8 mt-7 md:mt-12 text-sm">通常価格</p>
+              <p class="px-10 sp:px-8 mb-10 md:mb-12 text-base">{{ course.price.time120.toLocaleString() }}<span class="text-sm">円（税込）</span></p>
+              <a class="py-3 mx-auto w-4/5 block bg-gray-300 border-2 border-gray-300 rounded-lg text-white hover:text-gray-300 hover:bg-white" href="https://lin.ee/zTsXoxq" @click="homeAddLine()" target="_blank">LINEで予約する</a>
             </li>
           </ul>
         </li>
@@ -55,3 +58,16 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .btn-90::before {
+    content: '＼ おすすめ ／';
+    display: block;
+    font-size: 13px;
+    color: #f87171;
+    position: absolute;
+    top: -20px;
+    margin-left: -42.705px;
+    left: 50%;
+  }
+</style>
