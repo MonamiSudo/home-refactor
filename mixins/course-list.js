89c269998edd1315b57705cf9b4b3135653f2aa5
須藤百波 <sudomonami@gmail.com>
2023-01-30 14:24:11 +0900

    リバート｜価格表を元通りに戻す

--- mixins/course-list.js
+++ mixins/course-list.js
@@ -3,28 +3,7 @@ export const courseList = {
     return {
       courses: [
         {
-          category: '整体＋部分オイル',
-          name: '産後骨盤矯正コース/美容整体コース/快眠整体コース＋首肩or足の部分オイル',
-          description: '',
-          price: {
-            time60: 9900,
-            time90: 14300,
-            time120: 18700
-          }
-        },
-        {
-          category: '全身オイル',
-          name: '整体師の行う、全身オイルコース',
-          description: '',
-          price: {
-            time60: 11660,
-            time90: 16940,
-            time120: 22220
-          }
-        },
-        {
-          category: '全身整体',
-          name: '産後骨盤矯正コース/マタニティーコース/美容整体コース/快眠整体コース',
+          name: 'ぐっすり快眠整体コース',
           description: '',
           price: {
             time60: 9020,
@@ -32,33 +11,42 @@ export const courseList = {
             time120: 16940
           }  
         },
-        // {
-        //   name: '美容整体コース',
-        //   description: '',
-        //   price: {
-        //     time60: 9020,
-        //     time90: 12980,
-        //     time120: 16940
-        //   }
-        // },
-        // {
-        //   name: 'リモートワーク疲れ整体コース',
-        //   description: '',
-        //   price: {
-        //     time60: 9020,
-        //     time90: 12980,
-        //     time120: 16940
-        //   }
-        // },
-        // {
-        //   name: '産後骨盤矯正コース',
-        //   description: '',
-        //   price: {
-        //     time60: 9020,
-        //     time90: 12980,
-        //     time120: 16940
-        //   }
-        // },
+        {
+          name: '美容整体コース',
+          description: '',
+          price: {
+            time60: 9020,
+            time90: 12980,
+            time120: 16940
+          }
+        },
+        {
+          name: 'リモートワーク疲れ整体コース',
+          description: '',
+          price: {
+            time60: 9020,
+            time90: 12980,
+            time120: 16940
+          }
+        },
+        {
+          name: '産後骨盤矯正コース',
+          description: '',
+          price: {
+            time60: 9020,
+            time90: 12980,
+            time120: 16940
+          }
+        },
+        {
+          name: '整体師の行う、全身オイルコース',
+          description: '',
+          price: {
+            time60: 11660,
+            time90: 16940,
+            time120: 22220
+          }
+        }
       ]
     }
   },
